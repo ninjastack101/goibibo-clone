9396893644492c1f2fd27fd74b3edb51f396e1db
ninjastack101 <ninjastack101@gmail.com>
2019-09-13 18:41:33 +0530

    added autocomplete search component

---
+++ src/components/AutoCompleteSearch/AutoCompleteSearch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Select from 'react-select';
+
+export class AutoCompleteSearch extends React.Component {
+  state = {
+    selectedOption: null,
+  };
+
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    console.log(`Option selected:`, selectedOption);
+  };
+  render() {
+    const { selectedOption } = this.state;
+
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={this.props.options}
+      />
+    );
+  }
+}
