6a6ac7fd9cafeefff0b8f9c9147cd01317e61c46
ninjastack101 <ninjastack101@gmail.com>
2019-09-13 18:38:03 +0530

    added constants files

---
+++ src/constants/links.js
@@ -0,0 +1,6 @@
+export const APP_LOGO = "https://goibibo.ibcdn.com/styleguide/images/goLogo.png";
+export const ROOT_URL = "/"
+export const SEARCH_HOTEL_URL = "/hotels"
+export const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
+export const API_REQUEST_URL = `http://developer.goibibo.com/api/voyager/get_hotels_by_cityid/?app_id=${process.env.GOIBIBO_APP_ID}&app_key=${process.env.GOIBIBO_APP_KEY}&city_id=`;
+export const FETCH_CITY_URL = 'https://developer.goibibo.com/data/hotels_city_list.csv';
