e4ecf7dbd99881524dd40dc87ab4f1005ffd8f09
ninjastack101 <ninjastack101@gmail.com>
2019-09-19 15:36:57 +0530

    added router for connectedRouter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 import cities from './city';
+import hotels from './hotel';
 
-export default combineReducers({
-  cities
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  cities,
+  hotels,
 });
+
+export default createRootReducer;
