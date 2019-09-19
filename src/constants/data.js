6980cc63b9044de86f1602e24656efdfa224d932
ninjastack101 <ninjastack101@gmail.com>
2019-09-19 15:27:33 +0530

    added more constants

--- src/constants/data.js
+++ src/constants/data.js
@@ -1,9 +1,9 @@
-export const PERSISTENT_STORE_KEY = 'root'
+export const PERSISTENT_STORE_KEY = 'root';
 
 export const TAB_NAME = {
-  FLIGHT:'Flights',
-  BUS:'Bus',
-  HOTEL:'Hotels',
-}
+  FLIGHT: 'Flights',
+  BUS: 'Bus',
+  HOTEL: 'Hotels',
+};
 
 export const DECIMAL_BASE = 10;
--- src/constants/links.js
+++ src/constants/links.js
@@ -1,6 +1,7 @@
-export const APP_LOGO = "https://goibibo.ibcdn.com/styleguide/images/goLogo.png";
-export const ROOT_URL = "/"
-export const SEARCH_HOTEL_URL = "/hotels"
+export const APP_LOGO = 'https://goibibo.ibcdn.com/styleguide/images/goLogo.png';
+export const ROOT_URL = '/';
+export const HOTEL_FORM = '/hotels';
 export const CORS_URL = 'https://cors-anywhere.herokuapp.com/';
-export const API_REQUEST_URL = `http://developer.goibibo.com/api/voyager/get_hotels_by_cityid/?app_id=${process.env.GOIBIBO_APP_ID}&app_key=${process.env.GOIBIBO_APP_KEY}&city_id=`;
+export const HOTEL_BY_CITY = `http://developer.goibibo.com/api/voyager/get_hotels_by_cityid/?app_id=${process.env.REACT_APP_GOIBIBO_APP_ID}&app_key=${process.env.REACT_APP_GOIBIBO_APP_KEY}&city_id=`;
 export const FETCH_CITY_URL = 'https://developer.goibibo.com/data/hotels_city_list.csv';
+export const HOTEL_LIST_URL = '/hotels/find-hotels-in-city';
