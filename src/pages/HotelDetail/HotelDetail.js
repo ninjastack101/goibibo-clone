5431f42c51467b882e1e42f03d6794a791ffdcb1
ninjastack101 <ninjastack101@gmail.com>
2019-09-19 15:35:38 +0530

    added hotel detail main component

---
+++ src/pages/HotelDetail/HotelDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import HorizontalSearchbar from '../../components/HorizontalSearchBar/HorizontalSearchBar';
+
+function HotelDetail() {
+  return (
+    <div>
+      <Navbar />
+      <HorizontalSearchbar />
+    </div>
+  );
+}
+
+export default HotelDetail;
