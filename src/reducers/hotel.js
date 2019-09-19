ea7344292e6a8ffb664274d58d15b559c8fabee2
ninjastack101 <ninjastack101@gmail.com>
2019-09-19 15:56:10 +0530

    added hotel reducer

---
+++ src/reducers/hotel.js
@@ -0,0 +1,40 @@
+export const SET_HOTEL_CRITERIAS = 'SET_HOTEL_CRITERIAS';
+export const SET_HOTEL_LIST = 'SET_HOTEL_LIST';
+
+const initialState = {
+  list: [],
+  hotelCriteria: {
+    count: {
+      room: 1,
+      adult: 1,
+      children: 0,
+    },
+    checkInDate: new Date('2019-08-18T21:11:54'),
+    checkOutDate: new Date('2019-08-18T21:11:54'),
+  },
+};
+
+export default (state = initialState, action) => {
+  const { type } = action;
+  switch (type) {
+    case SET_HOTEL_LIST: {
+      const list = action.data;
+      return {
+        ...state, list,
+      };
+    }
+    case SET_HOTEL_CRITERIAS: {
+      const { count, checkInDate, checkOutDate } = action.data;
+      return {
+        ...state,
+        hotelCriteria: {
+          checkInDate,
+          checkOutDate,
+          count,
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
