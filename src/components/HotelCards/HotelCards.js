b2c1a4b92c06f96beb613cf90b356f17641c5474
ninjastack101 <ninjastack101@gmail.com>
2019-09-20 17:47:43 +0530

    added skeleton dummy divs

--- src/components/HotelCards/HotelCards.js
+++ src/components/HotelCards/HotelCards.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
 import './HotelCard.css';
 import Stars from '../Stars/Stars';
 import HotelLocation from './HotelLocation';
@@ -8,35 +10,45 @@ import HotelImage from './HotelImage';
 import HotelPrice from './HotelPrice';
 import HotelRating from './HotelRating';
 import HotelIcon from './HotelIcon';
+import { setSelectedHotel } from '../../actions/hotel.actions';
 
-const HotelCards = (props) => {
-  const { hotel } = props;
+function HotelCards(props) {
+  const { hotel, setSelectedHotel } = props;
+  const {
+    image, rating, name, location, price,
+  } = hotel;
   return (
-    <div className="card-wrapper">
+    <div className="card-wrapper" onClick={() => setSelectedHotel(hotel)}>
       <div className="width100 fl">
-        <HotelImage image={hotel.image} />
+        <HotelImage image={image} />
         <div className="detail-wrapper">
           <div className="left-details">
             <div className="fl padT2">
-              <Stars count={Number(hotel.rating)} />
+              <Stars count={Number(rating) || <Skeleton />} />
             </div>
             <HotelIcon />
-            <HotelName name={hotel.name} />
-            <HotelLocation location={hotel.location} />
+            <HotelName name={name || <Skeleton />} />
+            <HotelLocation location={location || <Skeleton count={3} />} />
           </div>
           <div className="vl" />
           <div className="right-details">
-            <HotelRating rating={hotel.rating} />
-            <HotelPrice price={hotel.price} />
+            <HotelRating rating={rating || <Skeleton />} />
+            <HotelPrice price={price || <Skeleton />} />
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
+
+
+const mapStateToProps = (state) => ({
+  hotelList: state.hotels.list,
+});
 
 HotelCards.propTypes = {
   hotel: PropTypes.objectOf(PropTypes.any).isRequired,
+  setSelectedHotel: PropTypes.func.isRequired,
 };
 
-export default HotelCards;
+export default (connect(mapStateToProps, { setSelectedHotel })(HotelCards));
--- src/pages/HotelList/HotelList.js
+++ src/pages/HotelList/HotelList.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { times } from 'lodash';
 import Navbar from '../../components/Navbar/Navbar';
 import HorizontalSearchbar from '../../components/HorizontalSearchBar/HorizontalSearchBar';
 import HotelCards from '../../components/HotelCards/HotelCards';
 import Filters from '../../components/Filters/Filters';
+import { DEFAULT_HOTEL_PROPS } from '../../constants/data';
 
 function HotelList(props) {
   const { hotelList } = props;
+
+  const renderHotelList = () => {
+    if (hotelList.length === 0) {
+      return times(2, DEFAULT_HOTEL_PROPS).map(
+        (hotel) => (<HotelCards hotel={hotel} />),
+      );
+    }
+
+    return hotelList.map(
+      (hotel) => (<HotelCards hotel={hotel} />),
+    );
+  };
+
   return (
     <div>
       <Navbar />
@@ -18,7 +34,7 @@ function HotelList(props) {
       </div>
       <Filters />
       {
-        hotelList.map((hotel) => (<HotelCards hotel={hotel} />))
+        renderHotelList()
       }
     </div>
   );
