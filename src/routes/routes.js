153d63e662c370926d776841cc2f58f19cd1eb20
ninjastack101 <ninjastack101@gmail.com>
2019-09-20 17:44:51 +0530

    changed routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,15 +4,17 @@ import {
 } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import HotelSearch from '../pages/HotelSearch/HotelSearch';
-import { HOTEL_FORM, HOTEL_LIST_URL } from '../constants/links';
+import { HOTEL_LIST_URL, HOTEL_DETAIL_URL, ROOT_URL } from '../constants/links';
 import HotelList from '../pages/HotelList/HotelList';
+import HotelDetail from '../pages/HotelDetail/HotelDetail';
 import { history } from '../store';
 
 const routing = (
   <ConnectedRouter history={history}>
     <Switch>
-      <Route exact path={HOTEL_FORM} component={withRouter(HotelSearch)} />
-      <Route exact path={HOTEL_LIST_URL} component={HotelList} />
+      <Route exact path={ROOT_URL} component={withRouter(HotelSearch)} />
+      <Route exact path={HOTEL_LIST_URL} component={withRouter(HotelList)} />
+      <Route exact path={HOTEL_DETAIL_URL} component={withRouter(HotelDetail)} />
     </Switch>
   </ConnectedRouter>
 );
